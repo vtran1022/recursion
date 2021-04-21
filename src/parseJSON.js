e63bc3d0ed34b868cc3a93aa332a7719ba3cbbcd
Vy Tran <vtran1022@Gmail.com>
2021-04-21 16:35:14 -0700

    implementing changes to code of parseJSON

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -3,5 +3,88 @@
 
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
-  // your code goes here
+  if (json[0] === 'n') {
+    return null;
+  } else if (json[0] === 't') {
+    return true;
+  } else if (json[0] === 'f') {
+    return false;
+  } else if (Number.isInteger(Number.parseInt(json))) {
+    return Number.parseInt(json);
+  } else if (json[0] === '"') {
+    return parseString(json);
+  } else if (json[0] === '{') {
+    return parseObject(json);
+  } else if (json[0] === '[') {
+    return parseArray(json);
+  }
 };
+
+/*
+
+object
+    {}
+    { members }
+members
+    pair
+    pair , members
+pair
+    string : value
+array
+    []
+    [ elements ]
+elements
+    value
+    value , elements
+value
+    string
+      if (json[0] === '"') {return parseString(json);}
+    number
+      if (Number.isInteger(Number.parseInt(json))) {return Number.parseInt(json);}
+    object
+      if (json[0] === '{') {return parseObject(json);}
+    array
+      if (json[0] === '[') {return parseArray(json);}
+    true
+      if (json[0] === 't') {return true;}
+    false
+      if (json[0] === 'f') {return false;}
+    null
+      if (json[0] === 'n') {return null;}
+
+*/
+
+// [4, "harro, world", [], {}];
+
+var parseString = function(json) {
+  var str = json.slice(1, json.length - 1);
+  return str;
+};
+
+var parseArray = function(json) {
+  var arr = [];
+  if (json.length <= 2) {
+    return arr;
+  }
+  var slicedStr = json.slice(1, json.length - 1);
+  if (slicedStr[0] === '[') {
+    return parseArray(slicedStr);
+  } else if (slicedStr[0] === '{') {
+    return parseObject(slicedStr);
+  } else if (slicedStr[0] === '"' && slicedStr.indexOf('"') === slicedStr.length - 1) {
+    return parseString(slicedStr);
+  } else {
+    var initialIndex = 0;
+    for (var i = 0; i <= slicedStr.length; i++) {
+      if (i === ',' || i === slicedStr.length) {
+        var innerSlicedStr = slicedStr.slice(initialIndex, i);
+        var slicedParse = parseJSON(innerSlicedStr);
+        arr.push(slicedParse);
+        initialIndex++;
+      }
+    }
+  }
+  return arr;
+};
+
+
